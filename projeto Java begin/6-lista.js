756413a95a3d59fe26a36971dbf453b5a933f8c9
Neto Vasconcellos <85071601+NetoVNG@users.noreply.github.com>
2021-09-16 14:27:42 -0300

    Criação de Arquivo
    Criado o arquivo 8-loops.js

--- projeto Java begin/6-lista.js
+++ projeto Java begin/6-lista.js
@@ -2,7 +2,7 @@ console.log(`Trabalhando com listas`);
 // const salvador = `Salvador`;
 // const saoPaulo = `São Paulo`;
 // const rioDeJaneiro = `Rio de Janeiro`;
- 
+
 const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
